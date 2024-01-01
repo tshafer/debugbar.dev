a6560a9527f53012787a0faa8926e3b765754acf
Julien Bourdeau <julien@julienbourdeau.com>
2024-01-01 10:29:25 +0100

    setup

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,11 @@ use TightenCo\Jigsaw\Jigsaw;
 /** @var \Illuminate\Container\Container $container */
 /** @var \TightenCo\Jigsaw\Events\EventBus $events */
 
+if (class_exists("Dotenv\Dotenv")) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->safeLoad();
+}
+
 /*
  * You can run custom code at different stages of the build process by
  * listening to the 'beforeBuild', 'afterCollections', and 'afterBuild' events.
